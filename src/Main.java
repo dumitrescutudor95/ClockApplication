a436322449b8947e8fb57cf7768064d2069029db
andreighinies <andreighinies@gmail.com>
2021-09-27 20:26:22 +0300

    i modified the message

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("The clock is starting x.x");
+        System.out.println("The clock is starting");
 
         for (int i = 0; i <= 100 ; i++) {
             System.out.println("TIC");
