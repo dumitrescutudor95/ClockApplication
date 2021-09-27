670bef3b60a42ed545664f6755438852c591bc15
andreighinies <andreighinies@gmail.com>
2021-09-27 20:19:18 +0300

    i added a text when clock is starting

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("The clock is starting x.x");
+
         for (int i = 0; i <= 100 ; i++) {
             System.out.println("TIC");
             System.out.println("TAC");
