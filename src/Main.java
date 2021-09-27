6b6f414f22552851d8637285073bc436df04bbe2
andreighinies <andreighinies@gmail.com>
2021-09-27 21:17:45 +0300

    i defined a person and i gave a clock with 3 secs interval

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
-    public static void main(String[] args)
-        throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
+        Person person = new Person(22, 178, "alb", new Clock(3));
         System.out.println("The clock is starting");
         for (int i = 0; i <= 100 ; i++) {
             System.out.println("TIC");
