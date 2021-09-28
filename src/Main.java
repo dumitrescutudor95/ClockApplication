13b3807abee6473cd655d818f363a79587fcf8da
andribunea <andribunea@gmail.com>
2021-09-28 17:54:17 +0300

    Added a scanner asking for the name and age

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         Person person = new Person(22, 178, "alb", new Clock(3));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        scanner.nextLine();
+        System.out.println("Enter age: ");
+        scanner.nextInt();
+
         System.out.println("The clock is starting");
         for (int i = 0; i <= 100 ; i++) {
             System.out.println("TIC");
