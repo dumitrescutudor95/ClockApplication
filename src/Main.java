26fd7714427ee6398ac78ba15a06fb1da5babe60
andreighinies <andreighinies@gmail.com>
2021-09-28 17:12:27 +0300

    i changed the seconds between TIC and TAC

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
         System.out.println("The clock is starting");
         for (int i = 0; i <= 100 ; i++) {
             System.out.println("TIC");
-            Thread.sleep(1000);
+            Thread.sleep(3000);
             System.out.println("SI");
             System.out.println("TAC");
-            Thread.sleep(1000);
+            Thread.sleep(3000);
         }
         System.out.println("Gata!");
     }
